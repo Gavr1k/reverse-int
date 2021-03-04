9e92329f4b31ced85cd42c9bdd05fa380aac296d
Gavr1k <47858531+Gavr1k@users.noreply.github.com>
2021-03-04 23:01:57 +0300

    feat: add function body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let buf = String(n);
+    let for_return = '';
+    for(let i = buf.length - 1; i > -1; i = i - 1)
+    {
+    	if(buf[i] == '-') continue;
+        for_return = for_return + buf[i];
+    }
+    return Number(for_return);
 }
